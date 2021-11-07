5d59466f727e53c194f7eb9d755c3f4a771eae0b
DaneshDhamejani <daneshdhamejani24@gmail.com>
2021-11-07 14:24:33 -0800

    Login implemented

--- backend/routes/Passenger.js
+++ backend/routes/Passenger.js
@@ -3,6 +3,7 @@ const passengerSchema = require("../models/Passenger");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
 
 router.post("/register", (req, res) => {
 	// Form validation
@@ -35,6 +36,40 @@ router.post("/register", (req, res) => {
 	  }
 	});
   });
+
+
+  router.post("/login", (req, res) => {
+    // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  const emailID = req.body.emailID;
+    const password = req.body.password;
+  // Find user by email
+  passengerSchema.findOne({ emailID }).then(user => {
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ emailnotfound: "Email not found" });
+      }
+  // Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User matched
+		  res.writeHead(200, {'Content-Type': 'text/plain'})
+		  res.end("Successful Login");
+         
+        } else {
+          return res
+            .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
+        }
+      });
+    });
+  });
+
+
 
 
 
