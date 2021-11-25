50f2c31ec52d8b7d487096fa4cded3f5035b1b33
Sohan Shirodkar <sohan1998@gmail.com>
2021-11-25 03:01:40 -0800

    Route changes

--- backend/routes/Booking.js
+++ backend/routes/Booking.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const passengerSchema = require("../models/Passenger");
-const bookingSchema = require("../models/Booking");
-const flightSchema = require("../models/Flight");
+const express = require('express');
+const passengerSchema = require('../models/Passenger');
+const bookingSchema = require('../models/Booking');
+const flightSchema = require('../models/Flight');
 const router = express.Router();
 
-router.post('/search',(req,res)=>{
+router.post('/search', (req, res) => {
+    flightSchema.find({ origin: req.body.origin, destination: req.body.destination }).exec((err, flightSchema) => {
+        if (err) {
+            res.json({ status: false, message: 'Error while searching' });
+        } else res.json({ flightSchema });
+    });
+});
 
-    flightSchema.find({ 'origin': req.body.origin, 'destination': req.body.destination }).exec((err, flightSchema) => {
-            if (err) {
-                res.json({ status: false, message: "Error while searching" })
-            }
-            else res.json({ flightSchema })
-        })
-})
-
-module.exports=router;
+module.exports = router;
--- backend/routes/Passenger.js
+++ backend/routes/Passenger.js
@@ -1,77 +1,68 @@
-const express = require("express");
-const passengerSchema = require("../models/Passenger");
+const express = require('express');
+const passengerSchema = require('../models/Passenger');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const validateRegisterInput = require("../validation/register");
-const validateLoginInput = require("../validation/login");
+const bcrypt = require('bcryptjs');
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
 
-router.post("/register", (req, res) => {
-	// Form validation
-  const { errors, isValid } = validateRegisterInput(req.body);
-  // Check validation
-	if (!isValid) {
-	  return res.status(400).json(errors);
-	}
-	passengerSchema.findOne({ emailID: req.body.emailID }).then(user => {
-	  if (user) {
-		return res.status(400).json({ email: "Email already exists" });
-	  } else {
-		const newUser = new passengerSchema({
-		  firstName: req.body.firstName,
-		  lastName: req.body.lastName,
-		  emailID: req.body.emailID,
-		  password: req.body.password
-		});
-  // Hash password before saving in database
-		bcrypt.genSalt(10, (err, salt) => {
-		  bcrypt.hash(newUser.password, salt, (err, hash) => {
-			if (err) throw err;
-			newUser.password = hash;
-			newUser
-			  .save()
-			  .then(user => res.json(user))
-			  .catch(err => console.log(err));
-		  });
-		});
-	  }
-	});
-  });
-
-
-  router.post("/login", (req, res) => {
+router.post('/register', (req, res) => {
     // Form validation
-  const { errors, isValid } = validateLoginInput(req.body);
-  // Check validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // Check validation
     if (!isValid) {
-      return res.status(400).json(errors);
+        return res.status(400).json(errors);
     }
-  const emailID = req.body.emailID;
-    const password = req.body.password;
-  // Find user by email
-  passengerSchema.findOne({ emailID }).then(user => {
-      // Check if user exists
-      if (!user) {
-        return res.status(404).json({ emailnotfound: "Email not found" });
-      }
-  // Check password
-      bcrypt.compare(password, user.password).then(isMatch => {
-        if (isMatch) {
-          // User matched
-		  res.writeHead(200, {'Content-Type': 'text/plain'})
-		  res.end("Successful Login");
-         
+    passengerSchema.findOne({ emailID: req.body.emailID }).then((user) => {
+        if (user) {
+            return res.status(400).json({ email: 'Email already exists' });
         } else {
-          return res
-            .status(400)
-            .json({ passwordincorrect: "Password incorrect" });
+            const newUser = new passengerSchema({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                emailID: req.body.emailID,
+                password: req.body.password,
+            });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then((user) => res.json(user))
+                        .catch((err) => console.log(err));
+                });
+            });
         }
-      });
     });
-  });
+});
 
+router.post('/login', (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateLoginInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const emailID = req.body.emailID;
+    const password = req.body.password;
+    // Find user by email
+    passengerSchema.findOne({ emailID }).then((user) => {
+        // Check if user exists
+        if (!user) {
+            return res.status(404).json({ emailnotfound: 'Email not found' });
+        }
+        // Check password
+        bcrypt.compare(password, user.password).then((isMatch) => {
+            if (isMatch) {
+                // User matched
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('Successful Login');
+            } else {
+                return res.status(400).json({ passwordincorrect: 'Password incorrect' });
+            }
+        });
+    });
+});
 
-
-
-
-
-module.exports=router;
+module.exports = router;
