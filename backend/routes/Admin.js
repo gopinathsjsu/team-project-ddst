a90f6955f20e453490564bc29b3be7f6c4ee9c49
unknown <dhruvinshah12@gmail.com>
2021-11-25 06:05:11 -0800

    DeleteFlight

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -90,4 +90,21 @@ router.post('/addFlights', (req, res) => {
     });
 });
 
+
+
+router.get('/deleteFlight', function(req, res) { 
+
+    flightSchema.findOneAndDelete({flightNumber:req.body.flightNumber}).then((deleteflight)=>{
+        // console.log(deleteflight);
+        if(deleteflight){
+            res.status(200).json({ message: 'Flight deleted sucessfully!' });
+        }
+        else{
+            res.status(400).json({ message: 'Flight does not exist!' });
+        }
+    })
+    .catch((err) => console.log(err));
+});
+
+
 module.exports = router;
