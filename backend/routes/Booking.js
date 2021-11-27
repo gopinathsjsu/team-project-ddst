35aba404cfe5ef2ea4d5d5577dbb72b1ec911e62
unknown <dhruvinshah12@gmail.com>
2021-11-27 02:39:38 -0800

    passengerDetails

--- backend/routes/Booking.js
+++ backend/routes/Booking.js
@@ -27,16 +27,81 @@ router.post('/selectFlight', async (req, res) => {
     });
 });
 
-router.post('/seatDetails', async (req, res) => {
+// router.post('/seatDetails', async (req, res) => {
+//     try {
+//         let passengerFirstName = req.body.passengerFirstName;
+//         let passengerLastName = req.body.passengerLastName;
+        // let seatNumber = req.body.seatNumber;
+        // let id = req.body.id;
+        // flightSchema.findOne({ _id: id }).then((selectFlight) => {
+        //     if (selectFlight) {
+        //         // console.log(selectFlight);
+        //         // res.json({ selectFlight });
+        //         let seatsAvailable = selectFlight.seatsAvailable;
+        //         let seatsTaken = selectFlight.seatsTaken;
+        //         // console.log(seatsAvailable);
+        //         for(var i=0;i<seatsAvailable.length;i++){
+        //             var tempSeat = seatsAvailable[i];
+        //             if(tempSeat==seatNumber){
+        //                 // console.log("tempSeat-------------------------");
+        //                 // seatsAvailable.pull(seatNumber);
+        //                 // seatsTaken.push(seatNumber);
+        //                 // console.log(seatsAvailable);
+        //                 // console.log(seatsTaken);
+
+        //                 flightSchema.findByIdAndUpdate({ _id: id  },{$set:{ seatsAvailable: seatsAvailable }});
+        //                 flightSchema.findByIdAndUpdate({ _id: id  },{$set:{ seatsTaken: seatsTaken }});
+        //                 res.json({ message: 'Seat added successfully' });
+        //                 break;
+        //             }
+        //             // else{
+        //             //     res.json({ message: 'Seat not available' });
+        //             // }
+        //         }
+        //     } else res.json({ status: false, message: 'Error while selecting!' });
+        // });
+        // let seatsAvailable = flightSchema
+        
+        // await flightSchema.updateOne({ seatsAvailable: seatNumber }, { $pull: { seatsAvailable: seatNumber } });
+        // await flightSchema.updateOne({ $push: { seatsTaken: seatNumber } });
+        // res.status(200).json({ message: 'Seat added successfully' });
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// });
+
+
+
+router.post('/passengerDetails', async (req, res) => {
     try {
         let passengerFirstName = req.body.passengerFirstName;
         let passengerLastName = req.body.passengerLastName;
         let seatNumber = req.body.seatNumber;
         await flightSchema.updateOne({ seatsAvailable: seatNumber }, { $pull: { seatsAvailable: seatNumber } });
-        await flightSchema.updateOne({ $push: { seatsTaken: seatNumber } });
-        res.status(200).json({ message: 'Seat added successfully' });
-        console.log();
-    } catch (error) {
+        let id = req.body.id;
+        flightSchema.findOne({ _id: id }).then((selectFlight) => {
+            if (selectFlight) {
+                const passengerDetail = new bookingSchema({
+                    passengerFirstName: passengerFirstName,
+                    passengerLastName: passengerLastName,
+                    flightNumber: selectFlight.flightNumber,
+                    airplaneName: selectFlight.airplaneName,
+                    origin: selectFlight.origin,
+                    destination: selectFlight.destination,
+                    seatNumber: seatNumber,
+                    price: selectFlight.price,
+                    startTime: selectFlight.startTime,
+                    endTime: selectFlight.endTime,
+                });
+                passengerDetail
+                .save()
+                .then((passengerDetail) => res.json(passengerDetail))
+                .catch((err) => console.log(err));
+            } else res.json({ status: false, message: 'Error while selecting!' });
+        });
+    }
+    catch (error) {
         console.log(error);
     }
 });
